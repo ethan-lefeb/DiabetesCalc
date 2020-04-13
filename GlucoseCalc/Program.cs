8291f9c02f6c02f5f945563794aea8b2938a8a8b
ethanlefeb <ethan.a.lefeb@gmail.com>
2020-04-13 12:59:48 -0400

    Add project files.

---
+++ GlucoseCalc/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlucoseCalc
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // Ratio adjusting. Convert to something that's saved ahead of time later.
+            Console.WriteLine("Please input your glucose-to-insulin ratio.");
+            int glucoseRatio = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please input your carbohydrate-to-insulin ratio.");
+            int carbohydrateRatio = Convert.ToInt32(Console.ReadLine());
+
+            // Asking for glucose and carbs.
+            Console.WriteLine("Input your current glucose, please!");
+            int patientGlucose = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Write your desired glucose, please!");
+            int desiredGlucose = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input the amount of carbohydrates you're about to eat!");
+            int mealCarbs = Convert.ToInt32(Console.ReadLine());
+
+
+            // The actual calculations are happening here.
+            
+            
+            int preventOverdose = desiredGlucose / glucoseRatio;
+            int glucoseUnits = patientGlucose / glucoseRatio - preventOverdose;
+            int carbohydrateUnits = mealCarbs / carbohydrateRatio;
+            int totalUnits = glucoseUnits + carbohydrateUnits;
+
+            // Showing the results of the calculations to the user.
+            Console.WriteLine("The total units of insulin you should take is: " + totalUnits);
+            Console.ReadLine();
+    }
+    }
+}
